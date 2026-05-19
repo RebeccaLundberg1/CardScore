26223231f249211f51f327a89a4f76def3db3581
Rebecca Lundberg <rebecca93lundberg@gmail.com>
2026-05-19 22:05:37 +0200

    Added NativeWind to write my style with

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
+import "./global.css";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! Hej Rebecca</Text>
+    <View className="flex-1 bg-white items-center justify-center">
+      <Text className="text-xl font-bold">CardScore</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
