8e40949ee59de865701e798f794c8b71f2023880
Rebecca Lundberg <rebecca93lundberg@gmail.com>
2026-05-21 15:55:14 +0200

    Added README, changed bg color to easy try light and dark mode

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,15 @@
 import { View, Text } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import GamesButton from '../components/GamesButton';
+import { colors } from '../theme';
 import games from '../data/games.json';
+
+const theme = colors.light;
 
 export default function HomeScreen() {
 
     return (
-    <View className="flex-1 bg-white items-center justify-center">
+    <View className='flex-1 items-center justify-center' style={{ backgroundColor: theme.bg }}>
       <Text className="text-xl font-bold">CardScore</Text>
       {games.filter(game => game.state == true).map(game => (
         <GamesButton key={game.id} label={game.name} />
---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+export const colors = {
+  light: {
+    bg: '#c9c9c9'
+  },
+  dark: {
+    bg: '#393939'
+  }
+}
